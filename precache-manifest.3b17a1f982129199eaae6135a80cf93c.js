c919758a24511de6e6bd72411aec61f712e72a45
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 10:49:21 +0200

    Updates

---
+++ precache-manifest.3b17a1f982129199eaae6135a80cf93c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a08fd4a73fc9917b42be404122a912",
+    "url": "/Lirten2/index.html"
+  },
+  {
+    "revision": "2c2734ce1012dba269b5",
+    "url": "/Lirten2/static/css/2.731ee94e.chunk.css"
+  },
+  {
+    "revision": "f2121cea602311db3c1e",
+    "url": "/Lirten2/static/css/main.0ea9daaa.chunk.css"
+  },
+  {
+    "revision": "2c2734ce1012dba269b5",
+    "url": "/Lirten2/static/js/2.5c85ec0d.chunk.js"
+  },
+  {
+    "revision": "f2121cea602311db3c1e",
+    "url": "/Lirten2/static/js/main.89863467.chunk.js"
+  },
+  {
+    "revision": "699c3221f6339f414301",
+    "url": "/Lirten2/static/js/runtime-main.f08fe0f3.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Lirten2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Lirten2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Lirten2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Lirten2/static/media/slick.f97e3bbf.svg"
+  }
+]);
